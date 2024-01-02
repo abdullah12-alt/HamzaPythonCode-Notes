3a79dab4770990fe3a6e9183c1cd106f646f5eff
Abdullah <hassanabdullah@gmail.com>
2024-01-02 18:42:22 +0500

    inheritence notes and code added

---
+++ inheritence.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, first_name,middle_name,last_name, age):
+        self.first_name=first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.age=age
+    
+    def __repr__(self):
+
+        return f"<Person first_name={self.first_name} last_name={self.last_name} age={self.age}> "
+    
+    def is_adult(self):
+        if self.age>=18:
+            return True
+        else:
+            return False
+class Student(Person): 
+    pass
+
+# p1=Person("Jan", "van" ,"Dijk",18)
+# print(p1)
+# print(p1.is_adult())
+
+s1=Student("John", "Doe" ,"John",17)
+print(s1)
+print(s1.is_adult())
